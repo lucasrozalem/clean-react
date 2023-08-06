284265bfbafd2c17569c181bc44769750b0ac1c8
Lucas Rozalem <lrozalem@gmail.com>
2023-08-06 12:36:27 -0300

    feat: inicia projeto

---
+++ src/infra/test/mock-axios.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { faker } from "@faker-js/faker";
+
+export const mockAxios = (): jest.Mocked<typeof axios> => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+  mockedAxios.post.mockResolvedValue({
+    data: faker.helpers.objectKey({ myProperty: "myValue" }),
+    status: faker.number.int(),
+  });
+
+  return mockedAxios;
+};
